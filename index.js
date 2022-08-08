86cfdf89b3371092ec21a3f29ebbcea5e4688aa9
barvax <69000340+barvax@users.noreply.github.com>
2022-08-08 07:23:12 +0300

    1

--- index.js
+++ index.js
@@ -61,17 +61,18 @@ function EnterBoxAtPos(r){
     root.insertAdjacentElement('afterend',box);
     let h1Title = document.createElement("h1");
     h1Title.id = 'h1Title'+counter;
-   
-    box.append(h1Title);
-    h1Title.innerHTML = box.boxObject.id;
     let nextBox= document.createElement("img");
-   
     nextBox.src = "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg";
     nextBox.classList.add('pic');
     box.append(nextBox);
     nextBox.addEventListener("click",()=>{
         EnterBoxAtPos(box.id)
     })
+    box.append(h1Title);
+    h1Title.innerHTML = box.boxObject.id;
+   
+   
+  
 
 h1Title.addEventListener("click", ()=>{
     delete arr[box.boxObject.id];
